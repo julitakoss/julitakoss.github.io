f6202379aa33d41ae317cc99268ff3e2f9409134
julita_koss@tlen.pl <julitakoss@github.com>
2020-05-19 17:17:21 +0200

    add changed at JS

--- 7.1-AJAX-pobierz-dane-programisty-github pages-/js/main.js
+++ 7.1-AJAX-pobierz-dane-programisty-github pages-/js/main.js
@@ -1,14 +1,14 @@
+let takeUrl = 'https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php'
 
-$(function () {
-    $('#download').click(function () {
-        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-            $('#dane-programisty').after(
-                '<br><div>Imię: ' + data.imie +
-                '<br> Nazwisko: ' + data.nazwisko +
-                '<br> Zawód: ' + data.zawod +
-                '<br> Firma: ' + data.firma
-            );
+$('#download').on('click', function()
+{
+  $("<div id='dane-programisty'>").appendTo('body');
 
-        });
-    });
+$.getJSON(takeUrl, function(data)
+{
+$('<p>').text(data.imie).appendTo('#dane-programisty');
+$('<p>').text(data.nazwisko).appendTo('#dane-programisty');
+$('<p>').text(data.zawod).appendTo('#dane-programisty');
+$('<p>').text(data.firma).appendTo('#dane-programisty');
 });
+})
